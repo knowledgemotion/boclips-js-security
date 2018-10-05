692d8f55a352d80d6b598beb987b4b176a7edb32
José Carlos Valero Sánchez <josecarlos@boclips.com>
2018-10-05 15:02:08 +0100

    [#160821196] Fix pipeline for js-security
    Signed-off-by: José Carlos Valero Sánchez <josecarlos@boclips.com>

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -8,7 +8,8 @@ cd ${app}
 npm i
 npm run test
 npm run build
-npm version "$(< version/boclips-js-security)"
+git checkout package-lock.json
+npm --no-git-tag-version -f version "$(< ../version/version)"
 )
 
 cp -R ${app}/dist/ ${app}/package.json ${app}/README.md  ${app}/.npmignore dist/
