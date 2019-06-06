3ea15a05a6a72b84e356340201e1ee45e48e587f
Andrew Bruce <me@andrewbruce.net>
2019-06-06 12:43:31 +0100

    Align with player pipeline / versioning

--- pipeline/test.sh
+++ pipeline/test.sh
@@ -20,9 +20,6 @@ npm run test
 npm run build
 
 ./run_e2e
-
-git checkout package-lock.json
-npm --no-git-tag-version -f version "$(< ../version/version)"
 )
 
 cp -R source/dist/ source/package.json source/README.md source/.npmignore dist/
