0574389d005e5e7b9737a5c6492ed3b17dce9136
Alex Glen <alex@boclips.com>
2019-07-08 11:22:16 +0100

    Add create-account to e2e for reliability
    [#166906890]
    
    Signed-off-by: Antony O'Neill <antony@boclips.com>

--- e2e/tests/BoclipsJsSecurity.spec.js
+++ e2e/tests/BoclipsJsSecurity.spec.js
@@ -1,21 +1,46 @@
-describe('Login journey', function () {
-    it('Logs-in and logs-out', function () {
-        cy.visit('http://localhost:8081').debug();
-        logIn();
+import uuid from 'uuid/v4';
 
-        cy.get('#header').then(node => node.text()).should('not.be.empty');
-        cy.wait(3000);
-        cy.get('#content').then(node => node.text()).should('not.be.empty');
+describe('Login journey', function() {
+  it('Creates account, logs in, and logs out', function() {
+    const username = `${uuid()}@boclips.com`;
+    const password = uuid();
 
-        cy.get('#logout').click();
+    cy.visit('http://localhost:8081').debug();
+    createAccount(username, password);
 
-        cy.get("#kc-form-login").then(node => node.text()).should('not.be.empty');
-    });
+    cy.visit('http://localhost:8081').debug();
+    logIn(username, password);
+
+    cy.get('#header')
+      .then(node => node.text())
+      .should('not.be.empty');
+    cy.wait(3000);
+    cy.get('#content')
+      .then(node => node.text())
+      .should('not.be.empty');
+
+    cy.get('#logout').click();
+
+    cy.get('#kc-form-login')
+      .then(node => node.text())
+      .should('not.be.empty');
+  });
 });
 
-function logIn() {
-    cy.get("#username").type(Cypress.env("USERNAME"));
-    cy.get("#password").type(Cypress.env("PASSWORD"));
-    cy.get("#kc-form-login").submit();
-    return this;
+function createAccount(username, password) {
+  cy.get('[data-qa="create-account"]').click();
+  cy.get('[data-qa="first-name"]').type('test');
+  cy.get('[data-qa="last-name"]').type('test');
+  cy.get('[data-qa="email"]').type(username);
+  cy.get('[data-qa="password"]').type(password);
+  cy.get('[data-qa="password-confirm"]').type(password);
+  cy.get('[data-qa="privacy-policy"]').click();
+  cy.get('[data-qa="register-button"]').click();
+}
+
+function logIn(username, password) {
+  cy.get('#username').type(username);
+  cy.get('#password').type(password);
+  cy.get('#kc-form-login').submit();
+  return this;
 }
