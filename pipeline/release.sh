c72a0d477fbfeefe0a071a27c5d9af13bd0e8180
Alex Glen <alex@boclips.com>
2019-07-09 14:34:19 +0100

    update publish script to check for changelog entry
    [#166906890]
    
    Signed-off-by: Antony O'Neill <antony@boclips.com>

--- pipeline/release.sh
+++ pipeline/release.sh
@@ -11,11 +11,16 @@ echo "This is $release_name" \
     > release/notes
 npm="npm --prefix $app"
 
+if [[ ! ${version} =~ "dev" && -z $(grep -F "## [${version}]" ${app}/CHANGELOG.md) ]]; then
+    echo "Changelog does not have an entry for this version"
+    exit 1
+fi
+
 $npm ci
 $npm run build
 $npm version "$version" \
     --no-git-tag-version \
     --force
 
-cp -R ${app}/dist/ ${app}/package.json ${app}/README.md dist/
+cp -R ${app}/dist/ ${app}/package.json ${app}/README.md ${app}/CHANGELOG.md ${app}/UPGRADING.md dist/
 
