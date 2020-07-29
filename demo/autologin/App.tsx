4163150c93e8d21db1fc29d51a5a8d95d35f47eb
Andrew Bruce <me@andrewbruce.net>
2020-07-29 12:28:46 +0100

    An autologin demo
    I intend to merge this with the main demo as a separate entrypoint next.
    This will let us run the Cypress tests on multiple 'apps'.
    
    [#173943675]

---
+++ demo/autologin/App.tsx
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import * as React from 'react';
+import { Component } from 'react';
+import * as ReactDom from 'react-dom';
+import BoclipsSecurity, {
+  AuthenticateOptions,
+} from '../../src/BoclipsSecurity';
+
+class Demo extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleLogOut = this.handleLogOut.bind(this);
+    this.state = { message: 'Not entered yet' };
+    this.username = React.createRef();
+    this.password = React.createRef();
+  }
+
+  public render(): React.ReactNode {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>
+              Username
+              <input ref={this.username} autoFocus />
+            </label>
+          </p>
+
+          <p>
+            <label>
+              Password
+              <input ref={this.password} type="password" />
+            </label>
+          </p>
+
+          <p>
+            <input type="submit" />
+          </p>
+        </form>
+
+        <p id="message">{this.state.message}</p>
+
+        <button onClick={this.handleLogOut}>Log out</button>
+      </>
+    );
+  }
+
+  handleLogOut() {
+    BoclipsSecurity.createInstance({
+      realm: 'boclips',
+      clientId: 'teachers',
+      requireLoginPage: false,
+      authEndpoint: 'https://login.staging-boclips.com/auth',
+    }).logout();
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    BoclipsSecurity.createInstance({
+      username: this.username.current.value,
+      password: this.password.current.value,
+      realm: 'boclips',
+      clientId: 'teachers',
+      requireLoginPage: false,
+      authEndpoint: 'https://login.staging-boclips.com/auth',
+      onLogin: (authenticated) => {
+        console.log('authed:', authenticated);
+        this.setState({
+          message: 'Successful authentication!',
+        });
+      },
+      onFailure: (error) => {
+        console.log('failure:', error);
+        this.setState({
+          message: 'Authentication failure!',
+        });
+      },
+    });
+  }
+}
+
+ReactDom.render(<Demo />, document.getElementById('content'));
